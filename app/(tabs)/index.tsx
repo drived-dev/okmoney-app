1430d89a4ec38577d2a834aef5aaad21a918ba8d
mels <chanatan.melmel@gmail.com>
2024-09-15 14:34:39 +0700

    feat: redo setup font

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,11 @@ import { Link } from "expo-router";
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { TEXT_STYLE } from "~/constants/Styles";
+import { cn } from "~/lib/utils";
 const Index = () => {
   return (
     <View>
-      <Text className={`${TEXT_STYLE.paragraph}`}>hello world</Text>
+      <Text className={cn(TEXT_STYLE.paragraph)}>hello world สวัสดี</Text>
     </View>
   );
 };
