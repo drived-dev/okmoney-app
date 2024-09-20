9e101835dbe2f1099c13b91cad2449141b8dc594
mels <chanatan.melmel@gmail.com>
2024-09-20 19:53:12 +0700

    feat: create StepperButtonGroup on dashboard.tsx

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -1,8 +1,8 @@
 import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { Input } from "~/components/ui/input";
-import { z } from "zod";
+import { Schema, TypeOf, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Label } from "~/components/ui/label";
 import {
@@ -14,10 +14,15 @@ import {
 import { Button } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 import { BUTTON } from "~/constants/Typography";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ArrowLeft, ArrowRight } from "lucide-react-native";
+import { CONTAINER } from "~/constants/Styles";
 const formSchema = z.object({
   name: z.string().min(2, { message: "ชื่อต้องมากกว่า 2 ตัวอักษร" }).max(50),
 });
+
+const Stack = createNativeStackNavigator();
 
 const History = () => {
   const {
@@ -30,35 +35,146 @@ const History = () => {
       name: "",
     },
   });
+  const [currentStep, setCurrentStep] = React.useState(0);
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     alert(values.name);
   }
 
   return (
-    <SafeAreaView>
-      <Controller
-        control={control}
-        name="name"
-        render={({ field: { onChange, onBlur, value } }) => (
-          <FormItem>
-            <FormLabel nativeID="email">อีเมล</FormLabel>
-            <Input
-              placeholder="โปรดใส่อีเมล"
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-            <FormDescription>hellow kdsfnjdsกหสกา่ด</FormDescription>
+    <StepContext.Provider
+      value={{ currentStep, setCurrentStep, onSubmit, lastStep: 1 }}
+    >
+      <NavigationContainer independent={true}>
+        <Stack.Navigator
+          initialRouteName="Page-0"
+          screenOptions={{
+            headerShown: false,
+            animation: "none",
+          }}
+        >
+          <Stack.Screen name="Page-0" component={InfoForm} />
+          <Stack.Screen name="Page-1" component={IForm} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StepContext.Provider>
+  );
+};
 
-            <FormMessage errorMessage={errors.name?.message}></FormMessage>
-          </FormItem>
-        )}
-      />
+type StepContextType = {
+  currentStep: number;
+  lastStep: number;
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+  onSubmit: any;
+  // TODO: change submit type
+};
 
-      <Button onPress={handleSubmit(onSubmit)}>
-        <Text className={cn(BUTTON.white)}>สวัสดี</Text>
+export const StepContext = React.createContext<StepContextType>({
+  currentStep: 0,
+  lastStep: 1,
+  setCurrentStep: () => {
+    console.error("oops, the default got used. Fix your bug!");
+  },
+  onSubmit: () => {},
+});
+
+const useStepContext = () => {
+  const stepContext = React.useContext(StepContext);
+
+  if (!stepContext) {
+    throw new Error("useStepContext must be used within a StepProvider");
+  }
+
+  return stepContext;
+};
+
+const StepperButtonGroup = ({ navigation }) => {
+  const { currentStep, setCurrentStep, onSubmit, lastStep } = useStepContext();
+
+  const isFirstStep = currentStep === 0;
+  const BackButton = () => {
+    if (isFirstStep) return null;
+
+    return (
+      <Button size="icon-lg" variant="ghost" onPress={navigateBack}>
+        <ArrowLeft color={"black"} />
       </Button>
+    );
+  };
+
+  const NextButton = () => {
+    if (currentStep < lastStep)
+      return (
+        <Button
+          size="xl"
+          className="flex flex-row justify-center items-center gap-2"
+          onPress={navigateToNextStep}
+        >
+          <Text className={cn(BUTTON.white)}>ถัดไป</Text>
+          <ArrowRight color={"white"} />
+        </Button>
+      );
+
+    return (
+      <Button
+        size="xl"
+        className="flex flex-row justify-center items-center gap-2"
+        onPress={onSubmit}
+      >
+        <Text className={cn(BUTTON.white)}>ส่งคำตอบ</Text>
+      </Button>
+    );
+  };
+
+  function navigateBack() {
+    const previousStep = currentStep - 1;
+    setCurrentStep(previousStep);
+
+    navigation.navigate(`Page-${previousStep}`);
+  }
+
+  function navigateToNextStep() {
+    const nextStep = currentStep + 1;
+    setCurrentStep(nextStep);
+
+    navigation.navigate(`Page-${nextStep}`);
+  }
+
+  return (
+    <View
+      className={cn(
+        "flex flex-row justify-between",
+        isFirstStep && "justify-end"
+      )}
+    >
+      <BackButton />
+      <NextButton />
+    </View>
+  );
+};
+
+const InfoForm = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <View className={cn(CONTAINER, "flex flex-col justify-between h-full")}>
+        <View>
+          <Text>Hello</Text>
+        </View>
+        <StepperButtonGroup navigation={navigation} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const IForm = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <View className={cn(CONTAINER, "flex flex-col justify-between h-full")}>
+        <View>
+          <Text>Hello2</Text>
+        </View>
+        <StepperButtonGroup navigation={navigation} />
+      </View>
     </SafeAreaView>
   );
 };
