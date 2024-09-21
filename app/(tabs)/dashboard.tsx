74a5dc2bf08d394bd14ae850d53ec5b49690bffe
mels <chanatan.melmel@gmail.com>
2024-09-21 18:01:11 +0700

    feat: finish step-form and demo on dashboard

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -8,58 +8,37 @@ import {
   FormProvider,
 } from "react-hook-form";
 import { Input } from "~/components/ui/input";
-import { Schema, TypeOf, z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Label } from "~/components/ui/label";
+import { z } from "zod";
 import {
   FormLabel,
   FormDescription,
   FormMessage,
   FormItem,
 } from "~/components/form";
-import { Button } from "~/components/ui/button";
-import { cn } from "~/lib/utils";
-import { BUTTON } from "~/constants/Typography";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ArrowLeft, ArrowRight } from "lucide-react-native";
-import { CONTAINER } from "~/constants/Styles";
-import { NavigationContext } from "@react-navigation/native";
+import { StepForm, StepFormScreen } from "~/components/form-stepper";
 
 const formSchema = [
-  // z.object({
-  //   name: z.string().min(2, { message: "ชื่อต้องมากกว่า 2 ตัวอักษร" }).max(50),
-  //   lastname: z
-  //     .string()
-  //     .min(2, { message: "นามสกุลต้องมากกว่า 2 ตัวอักษร" })
-  //     .max(50),
-  // }),
   z.object({
     email: z.string().min(2, { message: "ชื่อต้องมากกว่า 2 ตัวอักษร" }).max(50),
   }),
+  z.object({
+    name: z.string().min(2, { message: "ชื่อต้องมากกว่า 2 ตัวอักษร" }).max(50),
+  }),
 ];
-
-const Stack = createNativeStackNavigator();
 
 const History = () => {
   function onSubmit(values: z.infer<(typeof formSchema)[0]>) {
     alert(values);
+    console.log(values);
   }
 
-  const forms = [Form, Form];
+  const forms = [EmailForm, NameForm];
 
-  return <MultiStepForm onSubmit={onSubmit} />;
+  return <StepForm onSubmit={onSubmit} forms={forms} formSchema={formSchema} />;
 };
 
-const Form = ({ navigation }) => {
-  return (
-    <InfoForm navigation={navigation}>
-      <IForm></IForm>
-    </InfoForm>
-  );
-};
-
-const InfoForm = ({ navigation, children }) => {
+const EmailForm = ({ navigation }) => {
   const {
     control,
     handleSubmit,
@@ -69,196 +48,67 @@ const InfoForm = ({ navigation, children }) => {
     formState: { errors, isValid },
   } = useFormContext();
 
-  function validateInput() {
-    const schemaKeys = Object.keys(getValues());
-    trigger(schemaKeys);
-
-    return isValid;
-  }
-
-  function clearValidation() {
-    clearErrors();
-  }
-
   return (
-    <SafeAreaView>
-      <View className={cn(CONTAINER, "flex flex-col justify-between h-full")}>
-        {children}
-        <StepperButtonGroup
-          navigation={navigation}
-          validateInput={validateInput}
-          clearValidation={clearValidation}
+    <StepFormScreen navigation={navigation}>
+      <View>
+        <View>
+          <Text>Hello2</Text>
+        </View>
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <FormItem>
+              <FormLabel nativeID="email">อีเมล</FormLabel>
+              <Input
+                placeholder="โปรดใส่อีเมล"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+              <FormDescription>hellow kdsfnjdsกหสกา่ด</FormDescription>
+
+              <FormMessage errorMessage={errors.email?.message}>d</FormMessage>
+            </FormItem>
+          )}
         />
       </View>
-    </SafeAreaView>
+    </StepFormScreen>
   );
 };
 
-const IForm = ({}) => {
+const NameForm = ({ navigation }) => {
   const {
     control,
-    handleSubmit,
-    getValues,
-    clearErrors,
-    trigger,
-    formState: { errors, isValid },
+    formState: { errors },
   } = useFormContext();
 
   return (
-    // <SafeAreaView>
-    <View>
+    <StepFormScreen navigation={navigation}>
       <View>
-        <Text>Hello2</Text>
-      </View>
-      <Controller
-        control={control}
-        name="email"
-        render={({ field: { onChange, onBlur, value } }) => (
-          <FormItem>
-            <FormLabel nativeID="email">อีเมล</FormLabel>
-            <Input
-              placeholder="โปรดใส่อีเมล"
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-            />
-            <FormDescription>hellow kdsfnjdsกหสกา่ด</FormDescription>
-
-            <FormMessage errorMessage={errors.email?.message}>d</FormMessage>
-          </FormItem>
-        )}
-      />
-    </View>
-  );
-};
-
-const MultiStepForm = ({ forms, onSubmit }) => {
-  const [currentStep, setCurrentStep] = React.useState(0);
-  const currentSchema = formSchema[currentStep];
-
-  const method = useForm<z.infer<typeof currentSchema>>({
-    resolver: zodResolver(currentSchema),
-  });
-
-  return (
-    <FormProvider {...method}>
-      <StepContext.Provider
-        value={{ currentStep, setCurrentStep, onSubmit, lastStep: 1 }}
-      >
-        <NavigationContainer independent={true}>
-          <Stack.Navigator
-            initialRouteName="Page-0"
-            screenOptions={{
-              headerShown: false,
-              animation: "none",
-            }}
-          >
-            {forms.map((FormComponent, index) => (
-              <Stack.Screen
-                key={index}
-                name={`Page-${index}`}
-                component={FormComponent}
+        <View>
+          <Text>Hello2</Text>
+        </View>
+        <Controller
+          control={control}
+          name="name"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <FormItem>
+              <FormLabel nativeID="name">ชื่อ</FormLabel>
+              <Input
+                placeholder="โปรดใส่อีเมล"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
               />
-            ))}
-          </Stack.Navigator>
-        </NavigationContainer>
-      </StepContext.Provider>
-    </FormProvider>
-  );
-};
+              <FormDescription>hellow kdsfnjdsกหสกา่ด</FormDescription>
 
-type StepContextType = {
-  currentStep: number;
-  lastStep: number;
-  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
-  onSubmit: any;
-  // TODO: change submit type
-};
-
-export const StepContext = React.createContext<StepContextType>({
-  currentStep: 0,
-  lastStep: 1,
-  setCurrentStep: () => {
-    console.error("oops, the default got used. Fix your bug!");
-  },
-  onSubmit: () => {},
-});
-
-const useStepContext = () => {
-  const stepContext = React.useContext(StepContext);
-
-  if (!stepContext) {
-    throw new Error("useStepContext must be used within a StepProvider");
-  }
-
-  return stepContext;
-};
-
-const StepperButtonGroup = ({ navigation, validateInput, clearValidation }) => {
-  const { currentStep, setCurrentStep, onSubmit, lastStep } = useStepContext();
-
-  const isFirstStep = currentStep === 0;
-  const BackButton = () => {
-    if (isFirstStep) return null;
-
-    return (
-      <Button size="icon-lg" variant="ghost" onPress={navigateBack}>
-        <ArrowLeft color={"black"} />
-      </Button>
-    );
-  };
-
-  const NextButton = () => {
-    if (currentStep < lastStep)
-      return (
-        <Button
-          size="xl"
-          className="flex flex-row justify-center items-center gap-2"
-          onPress={navigateToNextStep}
-        >
-          <Text className={cn(BUTTON.white)}>ถัดไป</Text>
-          <ArrowRight color={"white"} />
-        </Button>
-      );
-
-    return (
-      <Button
-        size="xl"
-        className="flex flex-row justify-center items-center gap-2"
-        onPress={onSubmit}
-      >
-        <Text className={cn(BUTTON.white)}>ส่งคำตอบ</Text>
-      </Button>
-    );
-  };
-
-  function navigateBack() {
-    const previousStep = currentStep - 1;
-    clearValidation();
-
-    setCurrentStep(previousStep);
-    navigation.navigate(`Page-${previousStep}`);
-  }
-
-  function navigateToNextStep() {
-    const nextStep = currentStep + 1;
-    const isValid = validateInput();
-    if (!isValid) return;
-
-    setCurrentStep(nextStep);
-    navigation.navigate(`Page-${nextStep}`);
-  }
-
-  return (
-    <View
-      className={cn(
-        "flex flex-row justify-between",
-        isFirstStep && "justify-end"
-      )}
-    >
-      <BackButton />
-      <NextButton />
-    </View>
+              <FormMessage errorMessage={errors.name?.message}>d</FormMessage>
+            </FormItem>
+          )}
+        />
+      </View>
+    </StepFormScreen>
   );
 };
 
