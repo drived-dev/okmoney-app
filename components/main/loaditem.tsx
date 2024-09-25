4de358c86c56e4e87c0a2069213fa8e6878e34dd
FaRinZZZZ <steamihavemoney@gmail.com>
2024-09-25 20:44:27 +0700

    feat:fix div

--- components/main/loaditem.tsx
+++ components/main/loaditem.tsx
@@ -29,78 +29,84 @@ const LoanItem = ({ loan }: { loan: Loan }) => {
 
   return (
     // background deptor
-    <View className="bg-card p-3 my-1 rounded-3xl border border-border space-y-3">
+    <View className="bg-card p-3 my-1 rounded-3xl border border-border space-y-3 ">
       {/* Profile Image and Loan Info */}
-      <View className="flex-row items-center space-x-4">
-        {/* Profile Image */}
-        <Image
-          source={{ uri: loan.profileImage }}
-          className="w-12 h-12 rounded-full m-2 mb-4"
-        />
+      <View className="flex flex-col gap-2">
+        <View className="justify-between flex flex-row">
+          <View className="flex-row items-center space-x-4">
+            {/* Profile Image */}
+            <Image
+              source={{ uri: loan.profileImage }}
+              className="w-12 h-12 rounded-full"
+            />
 
-        {/* Loan Info */}
-        <View className="flex-1">
-          <Text className={cn(LABEL, "text-muted-foreground pl-2")}>
-            เลขสัญญา {loan.id}
-          </Text>
-          {/* Name: Bold nickname, gray full name */}
-          <Text className="text-foreground text-base font-bold font-ibm-medium pl-2 ">
-            {loan.nickname + "  "}
-            <Text className="text-muted-foreground font-ibm text-sm">
-              {loan.name}
-            </Text>
-          </Text>
-        </View>
+            {/* Loan Info */}
+            <View>
+              <Text className={cn(LABEL, "text-muted-foreground pl-2")}>
+                เลขสัญญา {loan.id}
+              </Text>
+              {/* Name: Bold nickname, gray full name */}
+              <Text className={cn(PARAGRAPH, "pl-2 ")}>
+                {loan.nickname + "  "}
+                <Text className="text-muted-foreground font-ibm text-sm">
+                  {loan.name}
+                </Text>
+              </Text>
+            </View>
+          </View>
 
-        {/* Loan Status */}
-        <View className={`px-3 py-2 rounded-2xl  ${statusColorbg}`}>
-          <Text
-            className={cn(
-              BUTTON,
-              `font-ibm-semibold text-destructive-foreground ${statusColortxt}`
-            )}
-          >
-            {loan.status}
-          </Text>
-        </View>
+          {/* Loan Status */}
+          <View className="flex-row flex">
+            <View
+              className={`px-3 py-2 rounded-2xl self-start ${statusColorbg}`}
+            >
+              <Text
+                className={cn(
+                  LABEL,
+                  `font-ibm-semibold text-destructive-foreground ${statusColortxt}`
+                )}
+              >
+                {loan.status}
+              </Text>
+            </View>
 
-        <View className="mx-1.5"></View>
-
-        <Button className="font-ibm-semibold text-muted-foreground pt-4 rounded-2xl bg-gray-200">
-          <Icon name="Ellipsis" size={30} color="#71717a" />
-        </Button>
-      </View>
-
-      {/* Outstanding Amount and Progress Bar with Total Amount on the Right */}
-      <View className="flex-row items-center space-x-2">
-        {/* Progress Bar */}
-        <View className="flex-1 h-6 bg-background rounded-lg relative border border-border ">
-          <View
-            className="absolute h-full bg-orange-500 rounded-md"
-            style={{ width: `${progress * 100}%` }}
-          />
-          <View className="absolute inset-0 flex-row justify-between items-center px-2">
-            {/* Outstanding Amount (left side inside the bar) */}
-            <Text className="text-orange-600 font-bold">
-              {loan.outstanding} บาท
-            </Text>
-            {/* Total Amount (right side inside the bar) */}
-            <Text className="text-gray-400 absolute right-2">
-              {loan.total} บาท
-            </Text>
+            <Button variant="ghost" size={"icon"}>
+              <Icon name="Ellipsis" size={24} color="#71717a" />
+            </Button>
           </View>
         </View>
 
-        <View className="px-2"></View>
+        {/* Outstanding Amount and Progress Bar with Total Amount on the Right */}
+        <View className="flex-row items-center space-x-2">
+          {/* Progress Bar */}
+          <View className="flex-1 h-6 bg-background rounded-lg relative border border-border ">
+            <View
+              className="absolute h-full bg-orange-500 rounded-md"
+              style={{ width: `${progress * 100}%` }}
+            />
+            <View className="absolute inset-0 flex-row justify-between items-center px-2">
+              {/* Outstanding Amount (left side inside the bar) */}
+              <Text className="text-orange-600 font-bold">
+                {loan.outstanding} บาท
+              </Text>
+              {/* Total Amount (right side inside the bar) */}
+              <Text className="text-gray-400 absolute right-2">
+                {loan.total} บาท
+              </Text>
+            </View>
+          </View>
 
-        {/* Due Date */}
-        <Text className={cn(TITLE, "text-muted-foreground text-sm mt-1")}>
-          ชำระทุก {loan.dueDate}
-        </Text>
+          <View className="px-2"></View>
+
+          {/* Due Date */}
+          <Text className={cn(TITLE, "text-muted-foreground text-sm mt-1")}>
+            ชำระทุก {loan.dueDate}
+          </Text>
+        </View>
       </View>
 
       {/* Action Buttons */}
-      <View className="flex-row justify-between items-center mt-3 space-x-2 ml-3 mb-2">
+      <View className="flex-row justify-between items-center mt-3 space-x-2 mb- gap-1">
         {/* Remind Button with Icon */}
         <Button className="bg-destructive-foreground flex-1 flex-row justify-center items-center border border-muted-foreground rounded-2xl py-2">
           <Icon name="Send" color="#71717a" size={22} />
@@ -109,10 +115,8 @@ const LoanItem = ({ loan }: { loan: Loan }) => {
           </Text>
         </Button>
 
-        <View className="px-1"></View>
-
         {/* Save Button */}
-        <Button className="flex-1 bg-destructive py-2 flex-row justify-center items-center mr-3 rounded-2xl">
+        <Button className="flex-1 bg-destructive py-2 flex-row justify-center items-center rounded-2xl">
           <Icon name="NotebookPen" color="white" size={22} />
           <Text
             className={cn(
