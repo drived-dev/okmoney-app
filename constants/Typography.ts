609e0d49aaad84b05462b760888e59bec16205b4
mels <chanatan.melmel@gmail.com>
2024-10-03 16:31:10 +0700

    feat: change letter spacing on typography

--- constants/Typography.ts
+++ constants/Typography.ts
@@ -1,8 +1,8 @@
 export const HEADER = "font-ibm-medium text-5xl"
-export const SUBHEADER = "font-ibm-medium text-[2.4rem]"
-export const TITLE = "font-ibm-medium text-2xl"
-export const PARAGRAPH = "font-ibm text-base leading-6"
+export const SUBHEADER = "font-ibm-medium text-[2.4rem] tracking-[1%] leading-[120%]"
+export const TITLE = "font-ibm-medium text-2xl tracking-[.7%]"
+export const PARAGRAPH = "font-ibm text-base leading-6 tracking-[0.2%]"
 export const PARAGRAPH_BOLD = "font-ibm-semibold text-base leading-[1.7rem]"
-export const LABEL = "font-ibm-medium text-sm leading-6"
+export const LABEL = "font-ibm-medium text-sm leading-6 tracking-[.3%]"
 
 export const  BUTTON = {black: "font-ibm-semibold text-black", white: "font-ibm-semibold text-white"}
