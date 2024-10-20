14a40ef3fa71cd542e3b27ae7b59f7e3c692b399
mels <chanatan.melmel@gmail.com>
2024-10-21 00:07:48 +0700

    feat: demo bottom sheet

---
+++ app/debtor/memo/[id].tsx
@@ -0,0 +1,48 @@
+import React, { useCallback, useMemo, useRef } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { router } from "expo-router";
+
+const App = () => {
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log(index, typeof index);
+    if (index === 0) {
+      bottomSheetRef.current.close();
+    }
+  }, []);
+
+  // renders
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheet
+        ref={bottomSheetRef}
+        onChange={handleSheetChanges}
+        enablePanDownToClose
+        backdropComponent={null}
+      >
+        <BottomSheetView style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "grey",
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: "center",
+  },
+});
+
+export default App;
