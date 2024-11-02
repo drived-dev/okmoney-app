26e1579897992fba48de73cc63b4fcba01147bb8
FaRinZZZZ <steamihavemoney@gmail.com>
2024-11-02 13:19:25 +0700

    fix: reset to base

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -24,7 +24,6 @@ import useLoanStore from "~/store/use-loan-store";
 import { Button } from "~/components/ui/button";
 import { Icon } from "~/components/icon";
 import { PARAGRAPH_BOLD, LABEL } from "~/constants/Typography";
-import { Drawer } from "react-native-drawer-layout";
 
 const Index = () => {
   const [isGridView, setIsGridView] = useState(false); // Toggle between GridView and ListView
