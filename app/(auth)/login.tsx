0e55b31b4f439faf43dc8e5605a482e3f0032e2f
mels <chanatan.melmel@gmail.com>
2024-11-03 22:58:53 +0700

    feat: done phone input form on (auth)/login

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -17,10 +17,34 @@ import PhoneInput from "~/components/phone-input";
 import { Button } from "~/components/ui/button";
 import { ThemeToggle } from "~/components/ThemeToggle";
 import { TouchableOpacity } from "react-native";
+import { z } from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormMessage } from "~/components/form";
+import { Input } from "~/components/ui/input";
+
+const phoneSchema = z.object({
+  phoneNumber: z.string().min(10),
+});
 
 const index = () => {
   const router = useRouter();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof phoneSchema>>({
+    resolver: zodResolver(phoneSchema),
+  });
   const [phoneNumber, setPhoneNumber] = React.useState("");
+
+  function onSubmit(data: z.infer<typeof phoneSchema>) {
+    router.push({
+      pathname: "/phone-login",
+      params: { phoneNumber: data.phoneNumber },
+    });
+  }
+
   // TODO: implement functionality
   return (
     <View className="h-full">
@@ -34,22 +58,24 @@ const index = () => {
               </Text>
             </View>
             <View />
-            <View className="flex flex-col gap-3">
+            <View className="flex flex-col gap-2">
               <View className={cn(GRID)}>
                 <Text className={cn(TITLE, "text-foreground")}>
                   เข้าร่วมผ่านเบอร์
                 </Text>
-                <PhoneInput value={phoneNumber} onChangeText={setPhoneNumber} />
-                <Button
-                  onPress={() =>
-                    router.push({
-                      pathname: "/phone-login",
-                      params: { phoneNumber },
-                    })
-                  }
-                >
+
+                <Controller
+                  control={control}
+                  name="phoneNumber"
+                  render={({ field: { onChange, value } }) => (
+                    <PhoneInput value={value} onChangeText={onChange} />
+                  )}
+                />
+                <FormMessage errorMessage={errors.phoneNumber?.message} />
+
+                <Button onPress={handleSubmit(onSubmit)}>
                   <Text className={cn(PARAGRAPH_BOLD, "text-background")}>
-                    ต่อไป
+                    ต่อไป {/* Remove error message from here */}
                   </Text>
                 </Button>
               </View>
