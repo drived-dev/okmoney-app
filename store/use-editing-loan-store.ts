3825647c5d4bcda862ce3b1b6400673f6c061770
FaRinZZZZ <steamihavemoney@gmail.com>
2025-05-01 14:39:38 +0700

    fix:edit

--- store/use-editing-loan-store.ts
+++ store/use-editing-loan-store.ts
@@ -2,17 +2,12 @@ import { create } from "zustand";
 
 interface EditingLoanState {
   id: string;
-  status: string;
 }
 
 const useEditingLoanStore = create<EditingLoanState>((set) => ({
   id: "",
-  status: "",
   setId: (id: string) => {
     set({ id });
-  },
-  setStatus: (status: string) => {
-    set({ status });
   },
 }));
 
